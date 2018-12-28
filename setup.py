00f37ac26d08baf93c7949f804745357f580d56d
Samuel Wilder <pebazium@gmail.com>
2018-12-28 15:33:16 -0500

    Add setup.py script and initial package

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Usage:
+pip install git+https://github.com/Pebaz/bloxel.git
+"""
+
+from distutils.core import setup
+
+setup(
+	name='bloxel',
+	version='1.0',
+	description='Isometric Voxel Generator',
+	author='Samuel Wilder',
+	packages=['bloxel'],
+	install_requires=[
+		'docopt',
+        'PIL'
+	]
+)
