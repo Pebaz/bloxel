4afcef087c7b4a542fc6c4cc31deeb2f5202d0f7
Samuel Wilder <pebazium@gmail.com>
2019-07-24 10:48:39 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
 	name='bloxel',
-	version='1.0',
+	version='1.1',
 	description='Isometric Voxel Generator',
 	author='Samuel Wilder',
 	packages=['bloxel'],
